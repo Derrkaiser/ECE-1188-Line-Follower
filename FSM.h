cf1519dd38cc4b9877895ffbd0b24d977e9bf953
grd23GrantDavis <77860562+grd23GrantDavis@users.noreply.github.com>
2022-03-20 18:17:40 -0400

    FSM Done

--- FSM.h
+++ FSM.h
@@ -8,8 +8,16 @@
 #ifndef FSM_H_
 #define FSM_H_
 
+/*
+Determine which motor function to call and call it
+using the states left and right duty parameters
+*/
+void motorOut(uint8_t dir,uint16_t dutyR,uint16_t dutyL);
 
-
-
+/*
+Output the current state and advance to the next one
+based on provided input
+*/
+void advanceState(void);
 
 #endif /* FSM_H_ */
